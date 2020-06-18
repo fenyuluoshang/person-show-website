71ce1af589c0e4b7d4727aa7b211a009e6618e8b
纷羽 <gzj0078@icloud.com>
2020-06-18 12:53:26 +0800

    更换加速域名

--- src/assets/cdns.ts
+++ src/assets/cdns.ts
@@ -1,21 +1,21 @@
 export const PrimaryIonDrive = process.env.NODE_ENV === "development" ?
-    require("./PrimaryIonDrive.glb") : "https://fenyu-media.oss-cn-beijing.aliyuncs.com/lab/model/PrimaryIonDrive.glb"
+    require("./PrimaryIonDrive.glb") : "https://fenyu-media.oss-accelerate.aliyuncs.com/lab/model/PrimaryIonDrive.glb"
 
 export const GO_IMG = process.env.NODE_ENV === "development" ?
-    require("./go-neusoft-edu-cn.png") : "https://fenyu-media.oss-cn-beijing.aliyuncs.com/lab/image/go-neusoft-edu-cn.png"
+    require("./go-neusoft-edu-cn.png") : "https://fenyu-media.oss-accelerate.aliyuncs.com/lab/image/go-neusoft-edu-cn.png"
 
-export const PARTY2019 = process.env.NODE_ENV === "development" ? require("./2019party.png") : "https://fenyu-media.oss-cn-beijing.aliyuncs.com/lab/image/2019party.png"
+export const PARTY2019 = process.env.NODE_ENV === "development" ? require("./2019party.png") : "https://fenyu-media.oss-accelerate.aliyuncs.com/lab/image/2019party.png"
 
-export const Admin_IMG = process.env.NODE_ENV === "development" ? require("./admin.png") : "https://fenyu-media.oss-cn-beijing.aliyuncs.com/lab/image/admin.png"
+export const Admin_IMG = process.env.NODE_ENV === "development" ? require("./admin.png") : "https://fenyu-media.oss-accelerate.aliyuncs.com/lab/image/admin.png"
 
-export const DBMT_IMG = process.env.NODE_ENV === "development" ? require("./dbmt-cn.png") : "https://fenyu-media.oss-cn-beijing.aliyuncs.com/lab/image/dbmt-cn.png"
+export const DBMT_IMG = process.env.NODE_ENV === "development" ? require("./dbmt-cn.png") : "https://fenyu-media.oss-accelerate.aliyuncs.com/lab/image/dbmt-cn.png"
 
-export const NEUSOFT_IMG = process.env.NODE_ENV === "development" ? require("./neusoft-edu-cn.png") : "https://fenyu-media.oss-cn-beijing.aliyuncs.com/lab/image/neusoft-edu-cn.png"
+export const NEUSOFT_IMG = process.env.NODE_ENV === "development" ? require("./neusoft-edu-cn.png") : "https://fenyu-media.oss-accelerate.aliyuncs.com/lab/image/neusoft-edu-cn.png"
 
-export const TOPPORS_IMG = process.env.NODE_ENV === "development" ? require("./toppros-cn.png") : "https://fenyu-media.oss-cn-beijing.aliyuncs.com/lab/image/toppros-cn.png"
+export const TOPPORS_IMG = process.env.NODE_ENV === "development" ? require("./toppros-cn.png") : "https://fenyu-media.oss-accelerate.aliyuncs.com/lab/image/toppros-cn.png"
 
-export const VISIT_IMG = process.env.NODE_ENV === "development" ? require("./visit.png") : "https://fenyu-media.oss-cn-beijing.aliyuncs.com/lab/image/visit.png"
+export const VISIT_IMG = process.env.NODE_ENV === "development" ? require("./visit.png") : "https://fenyu-media.oss-accelerate.aliyuncs.com/lab/image/visit.png"
 
-export const PHOTOMAKE_IMG = process.env.NODE_ENV === "development" ? require("./photo_make.jpg") : "https://fenyu-media.oss-cn-beijing.aliyuncs.com/lab/image/photo_make.jpg"
+export const PHOTOMAKE_IMG = process.env.NODE_ENV === "development" ? require("./photo_make.jpg") : "https://fenyu-media.oss-accelerate.aliyuncs.com/lab/image/photo_make.jpg"
 
-export const WISH_IMG = process.env.NODE_ENV === "development" ? require("./2020wish.png") : "https://fenyu-media.oss-cn-beijing.aliyuncs.com/lab/image/2020wish.png"
+export const WISH_IMG = process.env.NODE_ENV === "development" ? require("./2020wish.png") : "https://fenyu-media.oss-accelerate.aliyuncs.com/lab/image/2020wish.png"
