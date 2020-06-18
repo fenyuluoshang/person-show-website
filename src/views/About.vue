49ce1a5f465bcfb037ba55bf4a75d76b05074ca0
纷羽 <gzj0078@icloud.com>
2020-06-18 17:33:59 +0800

    .

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,13 +2,23 @@
   <div class="about page">
     <div class="page-box">
       <h2>关于</h2>
-      <div>
+      <div class="continue">
         <p>背景出自于 three.js 官方教程</p>
         <p>
           按钮动画出自于大魔法师
           <a target="_blank" href="https://flag.moe">哞菇</a> 大佬的网站
         </p>
+        <p></p>
       </div>
     </div>
   </div>
 </template>
+<style lang="scss" scoped>
+.continue {
+  font-size: 16px;
+  a {
+    color: inherit;
+    font-size: inherit;
+  }
+}
+</style>
