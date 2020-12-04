bc5b5fbd417d02bac192a41f2dedaa63c8d787c9
纷羽 <gzj0078@icloud.com>
2020-12-04 11:51:34 +0800

    更新sw规则

--- src/sw.js
+++ src/sw.js
@@ -3,17 +3,14 @@ workbox.core.setCacheNameDetails({ prefix: "person-show" });
 console.log(self.__precacheManifest)
 
 workbox.routing.registerRoute(
-  'https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js',
-  workbox.strategies.cacheFirst()
-)
-
-workbox.routing.registerRoute(
-  /^(https:)?\/\/fenyu-media\.oss-accelerate\.aliyuncs\.com\//,
+  ({url})=>{
+    return url.host == 'fenyu-media.oss-accelerate.aliyuncs.com'
+  },
   workbox.strategies.cacheFirst({
-    cacheName: 'cdn',
+    cacheName: 'oss-cdn',
     plugins: [
       new workbox.expiration.Plugin({
-        maxAgeSeconds: 7 * 24 * 60 * 60, // 这只最长缓存时间为 7 天
+        maxAgeSeconds: 7 * 24 * 60 * 60
       }),
     ],
   })
@@ -22,19 +19,12 @@ workbox.routing.registerRoute(
 workbox.routing.registerRoute(
   /^https:\/\/cdn\.jsdelivr\.net/,
   workbox.strategies.cacheFirst({
-    cacheName: 'cdn',
+    cacheName: 'js-cdn',
     plugins: [
       new workbox.expiration.Plugin({
-        maxAgeSeconds: 7 * 24 * 60 * 60, // 这只最长缓存时间为 7 天
+        maxAgeSeconds: 7 * 24 * 60 * 60
       }),
     ],
-  })
-)
-
-workbox.routing.registerRoute(
-  /.*\.(?:png|jpg|jpeg|svg|gif)/g,
-  workbox.strategies.cacheFirst({
-    cacheName: 'image-cache',
   })
 )
 
