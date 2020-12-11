d34767c74fec05c5213efd79b211e4fab8d0be3f
纷羽 <gzj0078@icloud.com>
2020-12-11 17:25:09 +0800

    ...

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,6 +2,12 @@
   <div class="about page">
     <div class="page-box">
       <h2>关于</h2>
+      <p class="small">
+        <router-link to="/">首页</router-link>&nbsp;
+        <a href="https://fenyu.club">主页</a>&nbsp;
+        <a href="https://blog.fenyu.club">我的博客</a>&nbsp;
+        <a href="https://github.com/fenyuluoshang">GitHub</a>&nbsp;
+      </p>
       <div class="continue">
         <p>背景出自于 three.js 官方教程</p>
         <p>
@@ -17,8 +23,18 @@
 .continue {
   font-size: 16px;
   a {
-    color: inherit;
     font-size: inherit;
   }
+}
+
+a {
+  color: inherit;
+  &:active {
+    color: inherit;
+  }
+}
+
+.small {
+  font-size: 88%;
 }
 </style>
